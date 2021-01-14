711b55d98cf69c384854af509d7ea886daa0a08f
Annie Wiegel <annewiegel17@gmail.com>
2021-01-14 02:22:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
+def fizzbuzz(num)
+  if num #divisible by 3
+    puts "Fizz"
+  elsif num #divisible by 5
+    puts "Buzz"
+  elsif num #divisible by both 3 and 5
+    puts "FizzBuzz"
+  elsif num #not divisble by 3 or 5
+    puts "nil"
+  end
 end
