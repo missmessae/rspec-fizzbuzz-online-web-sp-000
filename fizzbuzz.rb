8b422daee8ce3d51b6795efb3fc36bf193d1fac3
Annie Wiegel <annewiegel17@gmail.com>
2021-01-14 02:44:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,13 @@
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
-  elsif num % 5 == 0
+  end
+  if num % 5 == 0
     puts "Buzz"
-  elsif num % 3 == 0 and num % 5 == 0
+  end
+  if num % 3 == 0 and num % 5 == 0
     puts "FizzBuzz"
+  end
   else num % 3 == !0 and num % 5 == !0
     puts "nil"
   end
